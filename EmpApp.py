14a47eeb0d1d51be598602972ef111570088c545
JasonHo8801 <hojs-wp19@student.tarc.edu.my>
2022-09-20 12:19:15 +0800

    test run

--- EmpApp.py
+++ EmpApp.py
@@ -91,37 +91,37 @@ def manageAttendance():
 
     # later one by one append the html code together with the data to the string
 
-    # for row in result:          # row[] could be the data in the mySQL database
-    #     number = "<tr><td>%s</td>"%row[0]
-    #     p.append(number)
-    #     name = "<td>%s</td>"%row[1]
-    #     p.append(name)
-    #     empID = "<td>%s</td>"%row[2]
-    #     p.append(empID)
-    #     date = "<td>%s</td></tr>"%row[3]
-    #     p.append(date)
-    #     status = "<td>%s</td></tr>"%row[4]
-    #     p.append(status)
-    #     if (%row[5] == "checked")
-    #         attend = "<td><input type="checkbox" class="empAttend" name="emp_attendance" value="attend" checked></td>"
-    #     else
-    #         attend = "<td><input type="checkbox" class="empAttend" name="emp_attendance" value="attend"></td>"
+    for row in result:          # row[] could be the data in the mySQL database
+        number = "<tr><td>1. </td>" #"<tr><td>%s</td>"%row[0]
+        p.append(number)
+        name = "<td>%s</td>"%row[1] + %row[2]
+        p.append(name)
+        empID = "<td>%s</td>"%row[0]
+        p.append(empID)
+        date = "<td>%s</td></tr>"%row[8]
+        p.append(date)
+        status = "<td>%s</td></tr>"%row[6]
+        p.append(status)
+        if (%row[7] == "checked")
+            attend = "<td><input type="checkbox" class="empAttend" name="emp_attendance" value="attend" checked></td>"
+        else
+            attend = "<td><input type="checkbox" class="empAttend" name="emp_attendance" value="attend"></td>"
 
 
-    # contents = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-    # <html>
-    # <head>
-    # <meta content="text/html; charset=ISO-8859-1"
-    # http-equiv="content-type">
-    # <title>Python Webbrowser</title>
-    # </head>
-    # <body>
-    # <table>
-    # %s
-    # </table>
-    # </body>
-    # </html>
-    # '''%(p)
+    contents = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+    <html>
+    <head>
+    <meta content="text/html; charset=ISO-8859-1"
+    http-equiv="content-type">
+    <title>Python Webbrowser</title>
+    </head>
+    <body>
+    <table>
+    %s
+    </table>
+    </body>
+    </html>
+    '''%(p)
 
     # surround the %s with html file code that previously designed
 
