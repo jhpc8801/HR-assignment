8d0c7a536c468b930d575c7206f305542b44ec36
JasonHo8801 <hojs-wp19@student.tarc.edu.my>
2022-09-20 12:03:01 +0800

    trial

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('AddEmp.html')
+    return render_template('ManageAttendance.html')
 
 
 @app.route("/about", methods=['POST'])
