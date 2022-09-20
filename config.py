6b7efada25a6091588c2d52187211d17185a79b8
JasonHo8801 <77608340+JasonHo8801@users.noreply.github.com>
2022-09-20 11:34:48 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowchoonkeat-employee"
+customregion = "us-east-1"
