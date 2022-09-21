f7b8ad7a274c06dc63381c64c5a681a307b61ad1
JasonHo8801 <77608340+JasonHo8801@users.noreply.github.com>
2022-09-21 23:10:35 +0800

    update 3

--- EmpApp.py
+++ EmpApp.py
@@ -81,7 +81,7 @@ def AddEmp():
     print("all modification done...")
     return render_template('AddEmpOutput.html', name=emp_name)
 
-@app.route("/attend", methods=['GET', 'POST'])
+@app.route("/attend", methods=['GET'])
 def attendance():
     select_sql = "SELECT * FROM employee"
     cursor = db_conn.cursor()
