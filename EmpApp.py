bba6020023de9bd91d0141abb7351d0e258971a9
JasonHo8801 <77608340+JasonHo8801@users.noreply.github.com>
2022-09-24 09:59:23 +0800

    sat 27

--- EmpApp.py
+++ EmpApp.py
@@ -131,6 +131,8 @@ def GetEmpAtt():
     cursor2.close()
     cursor3.close()
 
+    status = false
+
     return render_template('ManageAttendance.html', id=emp_id, fname=first_name, lname=last_name, stat=status)
 
 @app.route("/attend", methods=['GET'])
