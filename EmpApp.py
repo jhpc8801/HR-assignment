d4dfe4390015c31717525db92a342cc3c0eb3e16
JasonHo8801 <77608340+JasonHo8801@users.noreply.github.com>
2022-09-24 18:28:21 +0800

    push 25

--- EmpApp.py
+++ EmpApp.py
@@ -118,6 +118,8 @@ def payrollList():
         arr[col].append(col + 1)
         arr[col].append(result[col][1] + result[col][2])
         arr[col].append(result[col][0])
+        arr[col].append(result[col][5])
+        arr[col].append(result[col][6])
         arr[col].append(result[col][7])
         arr[col].append(result[col][8])
 
